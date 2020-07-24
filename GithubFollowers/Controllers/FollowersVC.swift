0d0a28a071ef2d6fc4fff8b6a179f74c885d6574
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2020-07-23 19:22:58 -0700

    build showLoadingData activity indicator

--- GithubFollowers/Controllers/FollowersVC.swift
+++ GithubFollowers/Controllers/FollowersVC.swift
@@ -50,9 +50,10 @@ class FollowersVC: UIViewController {
     }
     
     private func getFollowers(page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] (result) in
             guard let self = self else { return }
-            
+            self.dismissLoadingView()
             switch result {
                 case .success(let followers):
                     if followers.count < 100 { self.hasMoreFollowers = false }
--- GithubFollowers/Extension/UIViewController+Ext.swift
+++ GithubFollowers/Extension/UIViewController+Ext.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
     func presentGFAlertOnTheMainThread(title: String, message: String, buttonTitle btnTitle: String) {
         DispatchQueue.main.async {
@@ -15,6 +17,32 @@ extension UIViewController {
             alert.modalPresentationStyle = .overFullScreen
             alert.modalTransitionStyle   = .crossDissolve
             self.present(alert, animated: true)
+        }
+    }
+    
+    func showLoadingView() {
+        containerView                   = UIView(frame: view.bounds)
+        containerView.backgroundColor   = .systemBackground
+        containerView.alpha             = 0
+        view.addSubview(containerView)
+        
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+        
+        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicatorView)
+        activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            activityIndicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        activityIndicatorView.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
